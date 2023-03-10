f5f196880cacc3403d553e6dffabc1d88ee611b5
Austin Montoya <austin@liminal.sh>
2023-03-10 11:09:00 -0800

    Austin/lmnl 12 create project skeleton (#1)
    * add next 13 starter app
    
    * move root into locale, setup redirects
    
    * add tailwind
    
    * add daisyui
    
    * get query working
    
    * remove built-in next styles
    
    * improve styling
    
    * remove superfluous styles
    
    * improve layout and routing
    
    * improve card styling
    
    * integrate daisy theme system
    
    ---------
    
    Co-authored-by: Austin Montoya <austin.r.montoya@gmail.com>

---
+++ next.config.js
@@ -0,0 +1,39 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:locale',
+        destination: '/:locale/default-channel'
+      },
+      {
+        source: '/:locale/c/:slug',
+        destination: '/:locale/default-channel/c/:slug'
+      }
+    ]
+  },
+  async redirects() { 
+    return [
+      {
+        source: '/',
+        destination: '/en-us',
+        permanent: false
+      }
+    ]
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'liminal-labs.saleor.cloud',
+        port: '',
+        pathname: '/media/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
---
+++ src/app/[locale]/[channel]/(home)/page.tsx
@@ -0,0 +1,16 @@
+import { ArrowPathIcon } from "@heroicons/react/24/solid";
+
+export default function HomePage() {
+  return (
+    <main className="container mx-auto my-auto w-full max-w-7xl h-full max-h-screen">
+      <h1 className="mt-12 flex flex-row justify-center items-center">
+        <span>
+          <ArrowPathIcon className="w-6 h-6 mr-2" />
+        </span>
+        <span className="text-3xl text-secondary">
+          Home Page Under Construction
+        </span>
+      </h1>
+    </main>
+  );
+}

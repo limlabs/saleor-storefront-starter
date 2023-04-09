d4fa8b2be1e8ae1308955a9bc5f074aa095398dc
Austin Montoya <126262824+austin-liminal@users.noreply.github.com>
2023-04-09 15:18:07 -0700

    add Github actions workflow (#13)
    * add gh actions workflow
    
    * fix build command
    
    * fix errors
    
    * pushing change
    
    * remove stuff
    
    * lose the 2s (for now)
    
    * revert changes to main
    
    * undo more thangs
    
    * get outta here yarn
    
    * fix build errors again
    
    * add project name
    
    * add prod override for main
    
    ---------
    
    Co-authored-by: ediazm <alcmaeon@gmail.com>

--- src/app/daisyui/badge/index.tsx
+++ src/app/daisyui/badge/index.tsx
@@ -2,25 +2,28 @@ import { forwardRef, PropsWithChildren } from "react";
 import clsx from "clsx";
 
 interface BadgeProps {
-    ghost?: boolean;
-    outline?: boolean;
-    className?: string;
+  ghost?: boolean;
+  outline?: boolean;
+  className?: string;
 }
 
-const Badge = forwardRef<HTMLDivElement, PropsWithChildren<BadgeProps>>(({
-    ghost,
-    outline,
-    className,
-    children,
-}, ref)=>{
-    const classNames= clsx('badge',{
-        'badge-outline': outline,
-        'badge-ghost': ghost,
-    }, className);
+const Badge = forwardRef<HTMLDivElement, PropsWithChildren<BadgeProps>>(
+  function Badge({ ghost, outline, className, children }, ref) {
+    const classNames = clsx(
+      "badge",
+      {
+        "badge-outline": outline,
+        "badge-ghost": ghost,
+      },
+      className
+    );
 
     return (
-        <div className={classNames} ref={ref}>{children}</div>
+      <div className={classNames} ref={ref}>
+        {children}
+      </div>
     );
-});
+  }
+);
 
 export default Badge;

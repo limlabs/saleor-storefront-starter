4df20fb47bc4575f8f7588e2410ae519bc96c9f7
Kyle McCall <kylem147@gmail.com>
2023-04-18 16:42:49 -0700

    adds two liminal color themes and three daisyui stock themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,4 +25,31 @@ module.exports = {
     },
   },
   plugins: [require("daisyui")],
+
+  //daisyUI config
+  daisyui: {
+    themes: [
+      {
+        liminalThemeBright: {
+          primary: "rgb(255, 21, 236)",
+          secondary: "rgb(5, 249, 249)",
+          accent: "rgb(81, 241, 113)",
+          neutral: "rgb(255, 245, 230)",
+          "base-100": "rgb(0, 0, 19)",
+        },
+      },
+      {
+        liminalThemeNeutral: {
+          primary: "rgb(255, 191, 244)",
+          secondary: "rgb(194, 255, 253)",
+          accent: "rgb(157, 255, 212)",
+          neutral: "rgb(255, 245, 230)",
+          "base-100": "rgb(0, 0, 19)",
+        },
+      },
+      "dark",
+      "light",
+      "synthwave",
+    ],
+  },
 };

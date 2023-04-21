1189714a456291f45e46132b099e266825806622
ediazm <alcmaeon@gmail.com>
2023-04-20 21:44:00 -0700

    Improved our homepage (#25)

--- src/app/[locale]/[channel]/(home)/page.tsx
+++ src/app/[locale]/[channel]/(home)/page.tsx
@@ -3,13 +3,8 @@ import { ArrowPathIcon } from '@heroicons/react/24/solid';
 export default function HomePage() {
   return (
     <main className="container mx-auto my-auto w-full max-w-7xl h-full max-h-screen">
-      <h1 className="mt-12 flex flex-row justify-center items-center">
-        <span>
-          <ArrowPathIcon className="w-6 h-6 mr-2" />
-        </span>
-        <span className="text-3xl text-secondary">
-          Home Page Under Construction
-        </span>
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+          Welcome to the Headless Store
       </h1>
     </main>
   );

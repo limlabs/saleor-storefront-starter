211fc9db59f50d32c5ee9f722a0a12b42ee7a7cd
Ivan Espinoza Hernandez <kuroisekai@gmail.com>
2023-04-22 17:44:37 -0700

    added unsaved changes

--- src/app/[locale]/[channel]/p/[slug]/page.tsx
+++ src/app/[locale]/[channel]/p/[slug]/page.tsx
@@ -13,14 +13,13 @@ export default async function Pdp({
   params: { locale, channel, slug },
 }: PageProps) {
   let { product } = await gqlClient.product({ slug });
-  let description = JSON.parse(product.description);
+
 
   return (
     <>
       <ProductHero
         locale={locale}
         channel={channel}
-        info={description?.blocks[0]?.data.text}
         product={product}
       />
     </>

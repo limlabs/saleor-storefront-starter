02f5edef9f368c92dc85284fb796e6e78d8c4a27
Kyle McCall <kylem147@gmail.com>
2023-04-24 18:55:15 -0700

    styling for lg screen breakpoint

--- src/app/[locale]/(components)/aboutMain.tsx
+++ src/app/[locale]/(components)/aboutMain.tsx
@@ -7,7 +7,7 @@ import Background from "../../../../public/window.jpg";
 
 export default function AboutMain() {
   return (
-    <div className="z-10 -sm:grid -sm:grid-rows-about-grid-mobile  justify-center justify-items-center items-center h-main-height ">
+    <div className="z-10 -sm:grid -sm:grid-rows-about-grid-mobile -lg:grid-cols-2 justify-center justify-items-center items-center h-main-height ">
       <div className="flex w-full h-full justify-center items-center mt-2">
         <Image
           src={Texture}
@@ -16,14 +16,21 @@ export default function AboutMain() {
           className="rounded-lg shadow-shadow-sm -lg:h-96 -lg:w-96"
         />
       </div>
-      <div className=" flex flex-col -sm:h-6/7 -lg:h-96 w-11/12 justify-center items-center bg-transparent shadow-shadow-sm rounded-md text-almost-black p-4 text-center">
-        <h3 className="font-press_start_2p">Our Mission</h3>
+      <div className=" flex flex-col -sm:h-6/7 -lg:h-96 w-11/12 -lg:12/13 -lg:mr-6 justify-center items-center bg-transparent shadow-shadow-sm rounded-md text-almost-black p-4 text-center">
+        <h3 className="font-press_start_2p -lg:text-xl -sm:mb-2 -lg:mb-4 ">
+          Our Mission
+        </h3>
         <p className="p-2 text-sm mb-2">
           Empowering your business with innovative technology, driven by the
           millennial spirit of creativity and collaboration.
         </p>
         <div className="mb-2">
-          <Image src={Slide} height={30} alt="slide" />
+          <Image
+            src={Slide}
+            height={30}
+            alt="slide"
+            className="-lg:h-10 -lg:w-10"
+          />
         </div>
         <p className="p-2 text-sm">
           Collectively we thrive on exploring the edges of what&apos;s possible.

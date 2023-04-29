89744a909efebf57ee3a56a61a5b3a8b8ea3f2a3
Ivan Espinoza Hernandez <kuroisekai@gmail.com>
2023-04-29 13:47:15 -0700

    removed unused filel and fixed merge issues

--- src/app/[locale]/(components)/productCard.tsx
+++ src/app/[locale]/(components)/productCard.tsx
@@ -8,14 +8,14 @@ import CardBody from "@/app/daisyui/card-body";
 import CardTitle from "@/app/daisyui/card-title";
 import CardActions from "@/app/daisyui/card-actions";
 import Badge from "@/app/daisyui/badge";
+import { useProductTranslation } from "@/core/client/useProductTranslation";
 import { ProductCardButton } from "./productCardButton";
 import { ProductRating } from "./productRating";
 import { LocaleLink } from "./localeLink";
 import { QuantitySelector } from "./quantitySelector";
 import { ProductPrice } from "./productPrice";
-import Indicator from "@/app/daisyui/indicator";
 import { ProductCardVariantList } from "./productCardVariantList";
-import { useProductTranslation } from "@/core/client/useProductTranslation";
+import Indicator from "@/app/daisyui/indicator";
 
 interface ProductCardProps {
   product: Product;
@@ -28,11 +28,6 @@ export const ProductCard: FC<ProductCardProps> = ({ product, animation }) => {
     thumbnail,
     pricing,
     slug,
-<<<<<<< HEAD
-=======
-    category,
->>>>>>> main
-    variants,
     rating,
   } = product;
   const { onSale } = pricing;
@@ -62,11 +57,7 @@ export const ProductCard: FC<ProductCardProps> = ({ product, animation }) => {
         className="absolute z-10 top-1 left-2"
       />
       <Badge className="absolute top-2 right-2 z-10 badge-accent">
-<<<<<<< HEAD
         {info.category.name}
-=======
-        {category.name}
->>>>>>> main
       </Badge>
       <LocaleLink
         href={`/p/${slug}`}
@@ -92,7 +83,7 @@ export const ProductCard: FC<ProductCardProps> = ({ product, animation }) => {
           <ProductCardVariantList
             className="absolute top-4 left-0 right-0"
             slug={slug}
-            variants={variants}
+            variants={info.variants}
             selected={variantID}
             onClick={setVarientID}
           />

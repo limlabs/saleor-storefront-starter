d614da1b4757310621ed2861d2c8a3d837648a85
Ivan Espinoza Hernandez <kuroisekai@gmail.com>
2023-05-05 18:18:35 -0700

    added translations to the files

--- src/core/server/i18n.ts
+++ src/core/server/i18n.ts
@@ -1,10 +1,19 @@
-import 'server-only';
+"use server";
 
 const i18n = {
-    'en-us': async ()=> (await import(`../../dictionaries/en.json`)).default,
-    'es-mx': async ()=> (await import(`../../dictionaries/es.json`)).default,
-}
+    'en-us': async () => (await import(`../../dictionaries/en-us.json`)).default,
+    'es-mx': async () => (await import(`../../dictionaries/es-mx.json`)).default,
+};
 
 export type Locale = keyof typeof i18n;
 
-export const getI18N = async (locale: Locale) => i18n[locale]();
+
+function _geti18NProp(path: string){
+    const pathParts = path.split('.');
+
+    // for()
+
+}
+export async function getI18N(locale: Locale) {
+    return i18n[locale]?.();
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,20 +2,44 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 const locales = ['en-us', 'es-mx'];
-const DEFAULT_LOCALE = 'en-us';
+const defaultLocale = 'en-us';
+
 const LOCALE_REGEX = /^\/[a-z]{2}-[a-z]{2,}/;
 
-export function middleware(request: NextRequest) {
+/**
+ * TODO: negotiate fallback locale here
+ * @param request 
+ * @returns 
+ */
+function getLocale(request: NextRequest) {
+    return defaultLocale;
+}
+
+function isInvalidLocale(locale?: string) {
+    return (!locale || locales.every(loc => `/${loc}` !== locale));
+}
+
+function findURlLocale(request: NextRequest) {
     const { pathname } = request.nextUrl;
-    const locale = pathname.match(LOCALE_REGEX)?.[0];
-    if (locale && locales.some(l => `/${l}` === locale)) return;
+    return pathname.match(LOCALE_REGEX)?.[0];
+}
 
-    return NextResponse.redirect(
-        new URL(locale ?
-            pathname.replace(locale, DEFAULT_LOCALE) :
-            `/${DEFAULT_LOCALE}/${pathname}`, request.url)
-    );
+export function middleware(request: NextRequest) {
 
+    let locale = findURlLocale(request);
+
+    if (isInvalidLocale(locale)) {
+        const userLocale = getLocale(request);
+        const { pathname, search } = request.nextUrl;
+        // const localeURL = locale ?
+        //     pathname.replace(locale, `/${userLocale}`) :
+        //     `/${userLocale}/${pathname}`;
+        const localeURL = `/${userLocale}/${pathname}`;
+
+        return NextResponse.redirect(
+            new URL(localeURL + search, request.url)
+        );
+    }
 }
 
 export const config = {

83ada110148ab50f7ea9316e99d69f798b02561a
Ivan Espinoza Hernandez <kuroisekai@gmail.com>
2023-05-14 20:18:21 -0700

    changed menus for collapse components

--- src/core/server/renderMenuItems.tsx
+++ src/core/server/renderMenuItems.tsx
@@ -1,21 +1,33 @@
 import { Fragment } from "react";
 import { IMenuItem_PropsFragment, IMenu_PropsFragment } from "@/gql/sdk";
 import { useMenuTranslation } from "./useMenuTranslation";
-import Menu from "@/app/daisyui/menu";
+import Collapse from "@/app/daisyui/collapse";
 
-interface Submenu extends IMenuItem_PropsFragment{
-    children?: IMenuItem_PropsFragment[] | null;
+interface Submenu extends IMenuItem_PropsFragment {
+  children?: IMenuItem_PropsFragment[] | null;
 }
 
-function renderCategory(item: IMenuItem_PropsFragment ) {
+function renderCategory(item: IMenuItem_PropsFragment) {
   const { name } = useMenuTranslation(item);
-  const children = (item as Submenu).children?.map(renderCategory) ??[];
-  return <li key={item.category?.id}>
-    <a>{name}</a>
-    <Menu className="bg-base-100">
-    {children}
-    </Menu>
-    </li>;
+  const children = (item as Submenu).children?.map(renderCategory) ?? [];
+  if (children?.length) {
+    return (
+      <Collapse
+        key={item.category?.id}
+        classNameContent="pr-0 border-secondary  border-solid peer-checked:border-l-[1px]"
+        classNameTitle="font-sm"
+        plus
+        title={name}
+      >
+        {children}
+      </Collapse>
+    );
+  }
+  return (
+    <li key={item.category?.id}>
+      <a>{name}</a>
+    </li>
+  );
 }
 
 export function renderMenuItems(menu?: IMenu_PropsFragment | null) {

0687ac49658a3398387f8dabd7b5f73a4116ff8b
Austin Montoya <austin@liminal.sh>
2023-05-23 23:11:59 -0700

    make single image look better for pdp

--- src/app/[locale]/(components)/productImageGrid.tsx
+++ src/app/[locale]/(components)/productImageGrid.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import Image from "next/image";
-import { IProductDetailsFragment, IProductVariantFragment } from "@/gql/sdk";
+import { IProductMedia, IProductVariantFragment } from "@/gql/sdk";
 import { FC } from "react";
 import { useProductSelection } from "@/core/client/useProductSelection";
-import CardMedia from "@/app/daisyui/card-media";
 
 /**
  * Considerations
@@ -17,6 +16,21 @@ interface ProductImageGridProps {
   initialVariant: IProductVariantFragment | null | undefined;
 }
 
+const SingleImage: FC<{ url?: string; alt?: string }> = ({ url, alt }) => {
+  return (
+    <div className="flex justify-center items-center pr-8 pt-2">
+      <Image
+        key={url}
+        src={url ?? ""}
+        alt={alt ?? ""}
+        width={840}
+        height={840}
+        className="bg-neutral block"
+      />
+    </div>
+  );
+};
+
 export const ProductImageGrid: FC<ProductImageGridProps> = ({
   initialVariant,
 }) => {
@@ -27,16 +41,21 @@ export const ProductImageGrid: FC<ProductImageGridProps> = ({
     return null;
   }
 
+  if (variant.media.length === 1) {
+    const { url, alt } = variant.media[0] as IProductMedia;
+    return <SingleImage url={url} alt={alt} />;
+  }
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
       {variant.media.map((media) => (
-        <CardMedia
+        <Image
           key={media.url}
-          accentBg
           src={media?.url ?? ""}
           alt={media?.alt ?? ""}
           width={420}
           height={420}
+          className="bg-neutral"
         />
       ))}
     </div>

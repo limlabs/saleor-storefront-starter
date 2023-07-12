921e59801560df452614d5ce0a6d5213816323ee
Kyle  McCall <kyle@liminal.sh>
2023-07-12 15:19:58 -0700

    adds padding to product description loading state

--- src/app/[locale]/[channel]/p/[slug]/loading.tsx
+++ src/app/[locale]/[channel]/p/[slug]/loading.tsx
@@ -4,10 +4,10 @@ import Badge from "@/app/daisyui/badge";
 export default function loading() {
   return (
     <main className="lg:flex lg:justify-between lg:mx-0 mt-4">
-      <section className="lg:w-2/3">
+      <section className="lg:w-2/3 ">
         <ProductImageGridSkeleton />
       </section>
-      <section className="lg:w-1/4 px-6 bg-almost-black glass rounded h-96 flex flex-col justify-start ml-2">
+      <section className="lg:w-1/4 px-6 lg:py-8 bg-almost-black glass rounded h-96 flex flex-col justify-start ml-2">
         <div className="text-2xl text-accent mt-6 lg:mt-0 ">
           <Badge className="mt-10 h-12 w-48 mix-blend-overlay"> </Badge>
         </div>
@@ -17,8 +17,10 @@ export default function loading() {
         <h1 className="text-4xl text-neutral my-2">
           <Badge className="mt-5 h-4 w-12 mix-blend-overlay"> </Badge>
         </h1>
-        <div className=" bg-slate-700 rounded-sm h-16"></div>
-        <Badge className="mt-5 h-16 w-24 mix-blend-overlay"> </Badge>
+        <div className=" bg-slate-700 rounded-sm h-16 w-24"></div>
+        <div>
+          <Badge className="mt-5 h-16 w-32 mix-blend-overlay rounded-md "></Badge>
+        </div>
       </section>
     </main>
   );

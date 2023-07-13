c12872f480b03a7b890b1e7aa27c26e4ff391204
kyle-liminal <kyle@liminal.sh>
2023-07-13 15:25:55 -0700

    sets up event handler for login and create account components

--- src/app/[locale]/auth/createAccount.tsx
+++ src/app/[locale]/auth/createAccount.tsx
@@ -1,35 +1,74 @@
+"use client";
 import Button from "@/app/daisyui/button";
 import TextInput from "@/app/daisyui/text-input";
+import { FormEvent, useState } from "react";
 
 export const CreateAccount = () => {
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="h-auto bg-slate-200 w-full flex flex-col items-center p-8">
       <h1 className="font-bold text-xl text-black">CREATE ACCOUNT</h1>
-      <section className="flex flex-col gap-4 text-black">
+      <form className="flex flex-col gap-4 text-black" onSubmit={handleSubmit}>
         <div className="flex flex-col">
-          <label>First Name</label>
-          <TextInput className="bg-white border border-black" />
+          <label htmlFor="firstName">First Name</label>
+          <TextInput
+            id="firstName"
+            className="bg-white border border-black"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
         </div>
         <div className="flex flex-col">
-          <label>Last Name</label>
-          <TextInput className="bg-white border border-black" />
+          <label htmlFor="lastName">Last Name</label>
+          <TextInput
+            id="lastName"
+            className="bg-white border border-black"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
         </div>
         <div className="flex flex-col">
-          <label>Email*</label>
-          <TextInput className="bg-white border border-black" />
+          <label htmlFor="email">Email*</label>
+          <TextInput
+            id="email"
+            className="bg-white border border-black"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div className="flex flex-col">
-          <label>Password*</label>
-          <TextInput className="bg-white border border-black" />
+          <label htmlFor="password">Password*</label>
+          <TextInput
+            id="password"
+            className="bg-white border border-black"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div className="flex flex-col">
-          <label>Confirm Password*</label>
-          <TextInput className="bg-white border border-black" />
+          <label htmlFor="confirmPassword">Confirm Password*</label>
+          <TextInput
+            id="confirmPassword"
+            className="bg-white border border-black"
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
         </div>
-      </section>
-      <div className="mt-4">
-        <Button>REGISTER</Button>
-      </div>
+        <div className="mt-4">
+          <Button>REGISTER</Button>
+        </div>
+      </form>
     </div>
   );
 };
--- src/app/[locale]/auth/login.tsx
+++ src/app/[locale]/auth/login.tsx
@@ -1,24 +1,44 @@
+"use client";
 import Button from "@/app/daisyui/button";
 import TextInput from "@/app/daisyui/text-input";
+import { FormEvent, useState } from "react";
 
 export const Login = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
   return (
     <div className="h-auto bg-slate-200 w-full flex flex-col items-center p-8">
-      <h1 className="font-bold text-xl text-black">LOGIN</h1>
-      <section className="flex flex-col gap-4 text-black">
-        <div className="flex flex-col">
-          <label>Email</label>
-          <TextInput className="bg-white border border-black" />
-        </div>
-        <div className="flex flex-col">
-          <label>Password</label>
-          <TextInput className="bg-white border border-black" />
-        </div>
-      </section>
+      <form onSubmit={handleSubmit}>
+        <h1 className="font-bold text-xl text-black">LOGIN</h1>
+        <section className="flex flex-col gap-4 text-black">
+          <div className="flex flex-col">
+            <label>Email</label>
+            <TextInput
+              className="bg-white border border-black"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label>Password</label>
+            <TextInput
+              className="bg-white border border-black"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </section>
 
-      <div className="mt-4">
-        <Button>LOG IN</Button>
-      </div>
+        <div className="mt-4">
+          <Button>LOG IN</Button>
+        </div>
+      </form>
+
       <h2 className="text-black">Forgot Password?</h2>
     </div>
   );

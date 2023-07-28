e6eaeeb0ae1656626e13f5435a8cb023b8fb0581
kyle-liminal <kyle@liminal.sh>
2023-07-28 15:16:40 -0700

    removes profile badge from secondary nav

--- src/app/[locale]/(components)/(header)/navDesktop.tsx
+++ src/app/[locale]/(components)/(header)/navDesktop.tsx
@@ -49,12 +49,6 @@ export const DesktopPrimaryNav: FC = () => {
           className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
         >
           <li>
-            <a className="justify-between">
-              Profile
-              <span className="badge">New</span>
-            </a>
-          </li>
-          <li>
             <Link href="/account-settings">Account Settings</Link>
           </li>
           <li>

efbfb86d365404cb2d073a6e732ba9d84dd9b92d
Ryan J. Parker <ryan.j.parker@wsu.edu>
2023-09-08 14:49:26 -0700

    add graphql mutation to set new password

--- src/app/[locale]/reset-password/page.tsx
+++ src/app/[locale]/reset-password/page.tsx
@@ -12,39 +12,49 @@ import { useState } from 'react';
 // TODO: write function that calls the graphql mutation named "setPassword"
 
 export default function ResetPassword() {
-  const [password, setPassword] = useState<string>('');
+  const [newPassword, setNewPassword] = useState<string>('');
   const [confirmPassword, setConfirmPassword] = useState<string>('');
   const [passwordError, setPasswordError] = useState<string>('');
   const [dirty, setDirty] = useState<boolean>(false);
-  const t = useTranslations();
 
   const handleSubmit = () => {
     const formData = new FormData();
+    formData.append('password', newPassword);
+    const respPromise = setPassword(formData);
+
+    respPromise.then((resp) => {
+      setPassword(formData);
+
+      // setIsSubmitted(true);
+    });
   };
 
   return (
     <div className="py-7 justify-start items-start gap-2.5 inline-flex">
       <div className="w-full md:w-1/2 p-10">
         <div className="flex flex-col justify-start items-start w-full gap-6">
-          <h1 className="text-base-10 text-4xl font-bold ">RESET PASSWORD</h1>
+          <h1 className="text-base-10 text-4xl font-bold ">
+            CREATE NEW PASSWORD
+          </h1>
           <form
             className="flex flex-col justify-start items-start gap-8 w-full"
             action={handleSubmit}
           >
             <TextField
-              id="loginEmail"
+              id="newPassword"
               name="email"
-              label="Create new password*"
+              label="New Password*"
+              type="password"
               className="p-3 bg-base-300 border border-neutral-800 justify-start items-start gap-3 inline-flex w-full"
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={(e) => setNewPassword(e.target.value)}
             />
             <TextField
-              label="Confirm new password*"
-              id="loginPassword"
+              label="Confirm Password*"
+              id="confirmPassword"
               name="password"
               type="password"
               className="p-3 bg-base-300 border border-neutral-800 justify-start items-start gap-3 inline-flex w-full"
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={(e) => setNewPassword(e.target.value)}
             />
             <Button variant="primary">SUBMIT</Button>
             <p>{passwordError}</p>
